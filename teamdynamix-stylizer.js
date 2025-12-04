abc54dd90c16c7d43dc8f00e1ea7611eb6154277
Caleb <caleb.buist@matsugov.us>
2025-12-04 09:25:23 -0900

    Add files

---
+++ teamdynamix-stylizer.js
@@ -0,0 +1,7 @@
+window.onload = (event) => {
+    const style = document.createElement('link');
+    style.href="https://matsugov.us/templates/msb_bolide/css/teamdynamix.css";
+    style.type="text/css";
+    style.rel="stylesheet";
+    document.head.append(style);
+};
