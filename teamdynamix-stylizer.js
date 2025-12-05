73eee774753b7f17f2bf30d71b527154b533c57c
Caleb Buist <caleb.buist@matsugov.us>
2025-12-05 08:52:02 -0900

    Update Style

--- teamdynamix-stylizer.js
+++ teamdynamix-stylizer.js
@@ -1,6 +1,6 @@
 window.onload = (event) => {
     const style = document.createElement('link');
-    style.href="https://matsugov.us/templates/msb_bolide/css/teamdynamix.css";
+    style.href="https://msb-tdx-stylizer.s3.us-west-2.amazonaws.com/teamdynamix.css";
     style.type="text/css";
     style.rel="stylesheet";
     document.head.append(style);
